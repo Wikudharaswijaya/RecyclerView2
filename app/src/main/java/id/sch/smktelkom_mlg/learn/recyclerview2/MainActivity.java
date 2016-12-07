58ece1b575ae9c785a5064578b09067f4c000d05
WikudharaSW <shofrina99@gmail.com>
2016-12-07 20:15:45 +0700

    Recycler View Grid

--- app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview2/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity
         recyclerView.setLayoutManager(layoutManager);
         mAdapter = new HotelAdapter(mList);
         recyclerView.setAdapter(mAdapter);
-
         fillData();
     }
 
